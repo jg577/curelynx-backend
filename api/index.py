ffc7a00626ab3fe6f26377a9cc0dbd225dd887ad
Jeevan Gyawali <jg577@cornell.edu>
2023-06-14 20:31:50 -0700

    Added max k=1

--- api/index.py
+++ api/index.py
@@ -36,7 +36,7 @@ vectorstore = Pinecone(index, openai_emb_service.embed_query, "text")
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 qa = ConversationalRetrievalChain.from_llm(
     llm=OpenAI(temperature=0),
-    retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
+    retriever=vectorstore.as_retriever(search_kwargs={"k": 1}),
     memory=memory,
 )
 
